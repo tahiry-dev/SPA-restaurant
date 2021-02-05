ff293b2063354996dde7a0cdf509aa4af23aee3f
tahiry-dev <tahirycontact@gmail.com>
2021-02-05 16:43:51 +0300

    add css-image and js loaders

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,33 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path].[name].[ext]'
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ]
+    }
 };
