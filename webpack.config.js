fb5008941afaa2e34fe0653291f1a51a6b0aa547
tahiry-dev <tahirycontact@gmail.com>
2021-02-06 08:51:39 +0300

    import bootstrap to the project

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,37 @@ module.exports = {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"],
             },
+
+
+            {
+                test: /\.(scss)$/,
+                use: [{
+                    // inject CSS to page
+                    loader: 'style-loader'
+                }, {
+                    // translates CSS into CommonJS modules
+                    loader: 'css-loader'
+                }, {
+                    // Run postcss actions
+                    loader: 'postcss-loader',
+                    options: {
+                        // `postcssOptions` is needed for postcss 8.x;
+                        // if you use postcss 7.x skip the key
+                        postcssOptions: {
+                            // postcss plugins, can be exported to postcss.config.js
+                            plugins: function () {
+                                return [
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    }
+                }, {
+                    // compiles Sass to CSS
+                    loader: 'sass-loader'
+                }]
+            }
+
         ]
     }
 };
